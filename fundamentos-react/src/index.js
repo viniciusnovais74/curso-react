6d021ed448a79493dc7539af326ecbfc8a405a02
viniciusnovais74 <viniciusnovais74@gmail.com>
2021-12-21 14:22:17 -0300

    Iniciando react - Primeiro Componente #1 Aula14

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import First from './components/basic/First';
+
+
+ReactDOM.render(
+    <div>
+        <First></First>
+    </div>,
+    document.getElementById('root')
+)
